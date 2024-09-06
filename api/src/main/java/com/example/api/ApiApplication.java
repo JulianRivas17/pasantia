695844218d1a758bb00596ef8e594886fb52d6bc
unknown <rivasaaron154@gmail.com>
2024-09-06 14:57:09 -0300

    Tu mensaje de commit aquí

---
+++ api/src/main/java/com/example/api/ApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.api;
+
+import com.example.api.resouce.PersonasCrontroller;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
---
+++ api/src/main/java/com/example/api/resouce/PersonasCrontroller.java
@@ -0,0 +1,23 @@
+package com.example.api.resouce;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/personas")
+public class PersonasCrontroller {
+
+    @GetMapping("/")
+    public List<String> personas() {
+        List<String> data = new ArrayList<>();
+        data.add("Jorge");
+        data.add("Raul");
+        data.add("Alejandra");
+        return data;
+    }
+}
