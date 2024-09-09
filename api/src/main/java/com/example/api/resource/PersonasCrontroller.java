44d84b91e94a70ca8471d936e1745ca2556727ad
Alejandra K. Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2024-09-09 10:27:37 -0300

    creacion de clase Persona endpoints POST y GET

--- api/src/main/java/com/example/api/resource/PersonasCrontroller.java
+++ api/src/main/java/com/example/api/resource/PersonasCrontroller.java
@@ -2,7 +2,9 @@ package com.example.api.resource;
 
 
 import com.example.api.domain.Persona;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,5 +25,15 @@ public class PersonasCrontroller {
         persona.setId(idPersona++);
         personas.add(persona);
         return persona;
+    }
+
+    @GetMapping("/{id}") //se establece que id será tratado como una variable segun se ingrese en la url
+    public Persona getPersonaById(@PathVariable Long id) {  //La anotación @PathVariable le dice a Spring que el valor de id debe extraerse de la parte correspondiente de la URL.
+        for (Persona persona : personas) { //Este es un bucle for-each que itera sobre la lista personas. En cada iteración, la variable persona representa un objeto Persona de la lista.
+            if (persona.getId().equals(id)) { //if id del objeto persona de Persona coincide con el id ingresado en la url
+                return persona; //retornará todos los datos del objeto persona de Persona
+            }
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Persona no encontrada"); //excepcion por si no se encuentra el id
     }
 }
